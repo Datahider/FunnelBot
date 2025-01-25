af290ced2c9c0b4721defa2591376a7ad7ecf7fa
drweb <drweb@DESKTOP-6245KLE>
2025-01-25 18:18:48 +0300

    Удалена поддержка пула групп для размещения задач

--- etc/logic.php
+++ etc/logic.php
@@ -7,5 +7,3 @@ Bot::addHandler(\losthost\FunnelBot\controller\callback\CallbackDone::class);
 Bot::addHandler(losthost\FunnelBot\controller\command\CommandStart::class);
 Bot::addHandler(\losthost\FunnelBot\controller\command\CommandSetHello::class);
 Bot::addHandler(losthost\FunnelBot\controller\command\CommandSetSubject::class);
-
-Bot::addHandler(\losthost\FunnelBot\controller\misc\MyChatMember::class);
